436291c08c262f1a008b13fad7a5b7b1051d98b1
ghettodimasta <dima_pavlov1702@mail.ru>
2023-06-21 15:01:44 +0300

    added celery to docker

--- fr_test_task/celery.py
+++ fr_test_task/celery.py
@@ -1,14 +1,18 @@
 from celery import Celery
 import os
 
+from fr_test_task import settings
+
 # Set the default Django settings module for the 'celery' program
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fr_test_task.settings')
 
 # Create the Celery app
-app = Celery('fr_test_task')
+app = Celery('fr_test_task',
+             broker=settings.CELERY_BROKER_URL,
+             backend=settings.CELERY_RESULT_BACKEND)
 
 # Load the celery config from Django settings
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Auto-discover tasks from all registered Django app configs
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
